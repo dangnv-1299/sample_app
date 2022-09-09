dd6f2eeb4348f84005a3a7fb573d9a6c241b144e
dangnv121299 <nguyen.van.dang@sun-asterisk.com>
2022-09-09 10:39:11 +0700

    fix find by

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   before_save{email.downcase!}
+  UPDATABLE_ATTRS = %i(name email password password_confirmation).freeze
   validates :name, presence: true, length: {maximum: Settings.user.max_name}
   validates :email, presence: true, length: {maximum: Settings.user.max_email},
                     format: {with: Settings.user.email_regex},
                     uniqueness: true
   has_secure_password
-  validates :password, presence: true, length: {minimum: Settings.user.min_password}
+  validates :password, presence: true,
+             length: {minimum: Settings.user.min_password}
 end

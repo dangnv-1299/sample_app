017f061bc29c56cd2a26b51a0bb0be80919abb94
dangnv121299 <nguyen.van.dang@sun-asterisk.com>
2022-09-14 09:44:18 +0700

    Chap 11 :Activation by Email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   before_save{email.downcase!}
+  before_create :create_activation_digest
   UPDATABLE_ATTRS = %i(name email password password_confirmation).freeze
   validates :name, presence: true, length: {maximum: Settings.user.max_name}
   validates :email, presence: true, length: {maximum: Settings.user.max_email},
@@ -31,13 +32,29 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  def authenticated? remember_token
-    return false if remember_digest.nil?
+  def authenticated? attribute, token
+    digest = send("#{attribute}_digest")
+    return false if digest.blank?
 
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    BCrypt::Password.new(digest).is_password? token
   end
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  def activate
+    update activated: true, activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  private
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
